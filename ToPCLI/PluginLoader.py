5c822cc696c0de7540e5c87a9ef6d2a5336cacb4
Andrew Tran <andremail03@gmail.com>
2019-10-12 18:33:39 -0700

    Implemented plugin loader

--- ToPCLI/PluginLoader.py
+++ ToPCLI/PluginLoader.py
@@ -1,3 +1,21 @@
+import sys
+import importlib
+import traceback
+
 class PluginLoader(object):
     def __init__(self):
-        pass
+        self.plugin_module = [ 'ToPCLI', 'plugin' ]
+
+    def load(self, args):
+        base = self.plugin_module + [ args[1] ]
+        plugin = base + [ args[2] ]
+        plugin_args = args[3:]
+
+        try:
+            base = importlib.import_module('.'.join(base))
+        except Exception as e:
+            traceback.print_tb(e.__traceback__)
+            print(e)
+            print('Unknown command "%s"' % args[1])
+            sys.exit(1)
+        base.run_plugin(plugin, plugin_args)
